d8c98999eda905a397bf1154f83e4bb1eea9080b
Gaurav <gauravsaini.728@gmail.com>
2021-09-27 22:57:12 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="CodinjaoftheWorld",
+    description="A small package for dvc ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CodinjaoftheWorld/DVC_ML",
+    author_email="gauravsaini.728@gmail.com",
+    package=["src"],
+    python_requires=">=3.7",
+    install_requires=[
+        'dvc',
+        'pandas',
+        'scikit-learn'
+    ]
+
+)
